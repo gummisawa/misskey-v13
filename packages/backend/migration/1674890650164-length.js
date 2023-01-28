bb82b958515b5a5b8004032192ee0fb2960b6f72
gummisawa <212nabari@gmail.com>
2023-01-28 16:50:43 +0900

    fix

--- packages/backend/migration/1674890650164-length.js
+++ packages/backend/migration/1674890650164-length.js
@@ -2,7 +2,6 @@ export class length1674890650164 {
     name = 'length1674890650164'
 
     async up(queryRunner) {
-			await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "username" TYPE character varying(4096)`);
 			await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "usernameLower" TYPE character varying(4096)`);
 			await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "name" TYPE character varying(4096)`);
     }
